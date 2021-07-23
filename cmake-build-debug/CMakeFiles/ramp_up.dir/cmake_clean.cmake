4ab596edc4ce91709c17ace793ceadab7af17a36
连玉捷 <lianyujie@lianyujiedeMacBook-Pro.local>
2021-07-23 23:07:56 +0800

    init the repo

---
+++ cmake-build-debug/CMakeFiles/ramp_up.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ramp_up.dir/main.cpp.o"
+  "ramp_up"
+  "ramp_up.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ramp_up.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
